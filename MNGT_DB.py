3c7f4f9dcfe2895db25a143cb5c2829d38c041d2
Beni_BGW <118313691+benibgw@users.noreply.github.com>
2025-04-27 14:55:59 -0300

    Add files via upload

--- MNGT_DB.py
+++ MNGT_DB.py
@@ -5,11 +5,11 @@ MNGT_DB = sqlite3.connect('MNGT_DB.db')
 cursor = MNGT_DB.cursor()
 
 cursor.execute('''
-     CREATE TABLE IF NOT EXISTS acessList (
+     CREATE TABLE IF NOT EXISTS accessList (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            acessCode INTEGER NOT NULL,
-            acessKey TEXT NOT NULL,
-            acessLevel TEXT NOT NULL
+            accessCode INTEGER NOT NULL,
+            accessKey TEXT NOT NULL,
+            accessLevel TEXT NOT NULL
     )
 ''')
 
@@ -18,9 +18,9 @@ cursor.execute('''
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
             age INTEGER NOT NULL,
-            cpf REAL NOT NULL,
+            cpf TEXT NOT NULL,
             email TEXT NOT NULL,
-            phone INTEGER NOT NULL,
+            phone TEXT NOT NULL,
             address TEXT NOT NULL
     )
 ''')
@@ -30,9 +30,9 @@ cursor.execute('''
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
             age INTEGER NOT NULL,
-            cpf REAL NOT NULL,
+            cpf TEXT NOT NULL,
             email TEXT NOT NULL,
-            phone INTEGER NOT NULL,
+            phone TEXT NOT NULL,
             position TEXT NOT NULL,
             salary REAL NOT NULL,
             address TEXT NOT NULL
@@ -75,17 +75,19 @@ cursor.execute('''
     )
 ''')
 
-def create_acess(acessCode, acessKey, acessLevel):
+def create_access(accessCode, accessKey, accessLevel):
     cursor.execute('''
-        INSERT INTO acessList (acessCode, acessKey, acessLevel)
-        VALUES (:acessCode, :acessKey, :acessLevel)''',
-        {'acessCode': acessCode, 'acessKey': acessKey, 'acessLevel': acessLevel})
+        INSERT INTO accessList (accessCode, accessKey, accessLevel)
+        VALUES (:accessCode, :accessKey, :accessLevel)''',
+        {'accessCode': accessCode, 'accessKey': accessKey, 'accessLevel': accessLevel})
+    MNGT_DB.commit()
 
 def register_client(name, age, cpf, email, phone, address):
     cursor.execute('''
         INSERT INTO clientList (name, age, cpf, email, phone, address)
         VALUES (:name, :age, :cpf, :email, :phone, :address)''',
         {'name': name, 'age': age, 'cpf': cpf, 'email': email, 'phone': phone, 'address': address})
+    MNGT_DB.commit()
     
 def register_employee(name, age, cpf, email, phone, position, salary, address):
     cursor.execute('''
@@ -93,12 +95,14 @@ def register_employee(name, age, cpf, email, phone, position, salary, address):
         VALUES (:name, :age, :cpf, :email, :phone, :position, :salary, :address)''',
         {'name': name, 'age': age, 'cpf': cpf, 'email': email, 'phone': phone,
          'position': position, 'salary': salary, 'address': address})
+    MNGT_DB.commit()
     
 def register_product(name, price, quantity):
     cursor.execute('''
         INSERT INTO productList (name, price, quantity)
         VALUES (:name, :price, :quantity)''',
         {'name': name, 'price': price, 'quantity': quantity})
+    MNGT_DB.commit()
     
 def register_sale(client_id, employee_id, product_id, quantity, total_price, date):
     cursor.execute('''
@@ -106,6 +110,7 @@ def register_sale(client_id, employee_id, product_id, quantity, total_price, dat
         VALUES (:client_id, :employee_id, :product_id, :quantity, :total_price, :date)''',
         {'client_id': client_id, 'employee_id': employee_id, 'product_id': product_id,
          'quantity': quantity, 'total_price': total_price, 'date': date})
+    MNGT_DB.commit()
     
 def register_service(client_id, employee_id, service_type, service_price, service_date):
     cursor.execute('''
@@ -114,15 +119,17 @@ def register_service(client_id, employee_id, service_type, service_price, servic
         {'client_id': client_id, 'employee_id': employee_id,
          'service_type': service_type, 'service_price': service_price,
          'service_date': service_date})
+    MNGT_DB.commit()
     
-def edit_acess(acessCode, acessKey, acessLevel, id):
+def edit_access(accessCode, accessKey, accessLevel, id):
     cursor.execute('''
-        UPDATE acessList
-        SET acessCode = :acessCode,
-            acessKey = :acessKey,
-            acessLevel = :acessLevel
+        UPDATE accessList
+        SET accessCode = :accessCode,
+            accessKey = :accessKey,
+            accessLevel = :accessLevel
         WHERE id = :id''',
-        {'acessCode': acessCode, 'acessKey': acessKey, 'acessLevel': acessLevel, 'id': id})
+        {'accessCode': accessCode, 'accessKey': accessKey, 'accessLevel': accessLevel, 'id': id})
+    MNGT_DB.commit()
     
 def edit_client(name, age, cpf, email, phone, address, id):
     cursor.execute('''
@@ -135,6 +142,7 @@ def edit_client(name, age, cpf, email, phone, address, id):
             address = :address
         WHERE id = :id''',
         {'name': name, 'age': age, 'cpf': cpf, 'email': email, 'phone': phone, 'address': address, 'id': id})
+    MNGT_DB.commit()
     
 def edit_employee(name, age, cpf, email, phone, position, salary, address, id):
     cursor.execute('''
@@ -150,6 +158,7 @@ def edit_employee(name, age, cpf, email, phone, position, salary, address, id):
         WHERE id = :id''',
         {'name': name, 'age': age, 'cpf': cpf, 'email': email, 'phone': phone,
          'position': position, 'salary': salary, 'address': address, 'id': id})
+    MNGT_DB.commit()
     
 def edit_product(name, price, quantity, id):
     cursor.execute('''
@@ -159,6 +168,7 @@ def edit_product(name, price, quantity, id):
             quantity = :quantity
         WHERE id = :id''',
         {'name': name, 'price': price, 'quantity': quantity, 'id': id})
+    MNGT_DB.commit()
     
 def edit_sale(client_id, employee_id, product_id, quantity, total_price, date, id):
     cursor.execute('''
@@ -172,6 +182,7 @@ def edit_sale(client_id, employee_id, product_id, quantity, total_price, date, i
         WHERE id = :id''',
         {'client_id': client_id, 'employee_id': employee_id, 'product_id': product_id,
          'quantity': quantity, 'total_price': total_price, 'date': date, 'id': id})
+    MNGT_DB.commit()
     
 def edit_service(client_id, employee_id, service_type, service_price, service_date, id):
     cursor.execute('''
@@ -185,45 +196,52 @@ def edit_service(client_id, employee_id, service_type, service_price, service_da
         {'client_id': client_id, 'employee_id': employee_id,
          'service_type': service_type, 'service_price': service_price,
          'service_date': service_date, 'id': id})
+    MNGT_DB.commit()
     
-def delete_acess(id):
+def delete_access(id):
     cursor.execute('''
-        DELETE FROM acessList
+        DELETE FROM accessList
         WHERE id = :id''',
         {'id': id})
+    MNGT_DB.commit()
     
 def delete_client(id):
     cursor.execute('''
         DELETE FROM clientList
         WHERE id = :id''',
         {'id': id})
+    MNGT_DB.commit()
     
 def delete_employee(id):
     cursor.execute('''
         DELETE FROM employeeList
         WHERE id = :id''',
         {'id': id})
+    MNGT_DB.commit()
 
 def delete_product(id):
     cursor.execute('''
         DELETE FROM productList
         WHERE id = :id''',
         {'id': id})
+    MNGT_DB.commit()
     
 def delete_sale(id):
     cursor.execute('''
         DELETE FROM salesList
         WHERE id = :id''',
         {'id': id})
+    MNGT_DB.commit()
     
 def delete_service(id):
     cursor.execute('''
         DELETE FROM serviceList
         WHERE id = :id''',
         {'id': id})
+    MNGT_DB.commit()
     
-def get_all_access():
-    cursor.execute('SELECT * FROM acessList')
+def get_all_acccess():
+    cursor.execute('SELECT * FROM accessList')
     return cursor.fetchall()
 
 def get_all_clients():
@@ -245,5 +263,12 @@ def get_all_sales():
 def get_all_services():
     cursor.execute('SELECT * FROM serviceList')
     return cursor.fetchall()
+
+def check_access(accessCode, accessKey):
+    cursor.execute('''
+        SELECT * FROM accessList
+        WHERE accessCode = :accessCode AND accessKey = :accessKey''',
+        {'accessCode': accessCode, 'accessKey': accessKey})
+    return cursor.fetchone()
 
 MNGT_DB.commit()
--- interface.py
+++ interface.py
@@ -1,19 +1,60 @@
 import tkinter as tk
 from tkinter import messagebox
-from MNGT_DB import MNGT_DB
+from MNGT_DB import check_access
 
-root = tk.Tk()
-root.title("MNGT System")
-window_width = 1280
-window_height = 720
+def acessWindow():
+    root = tk.Tk()
+    root.title("MNGT System")
+    window_width = 275
+    window_height = 200
 
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
 
-x = (screen_width // 2) - (window_width // 2)
-y = (screen_height // 2) - (window_height // 2)
-root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+    x = (screen_width // 2) - (window_width // 2)
+    y = (screen_height // 2) - (window_height // 2)
+    root.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
+    def access(): 
+        accessCode = textbox1.get()
+        accessKey = textbox2.get()
+        result = check_access(accessCode, accessKey)
+        if result == None:
+            messagebox.showerror("Error", "Access code or access key is incorrect")
+        else:
+            messagebox.showinfo("Success", "Access granted")
+            root.destroy()
+            mainWindow()
+    
+    label1 = tk.Label(root, text="Type your access code and access key",)
+    label1.grid(row=0, column=1, padx=10, pady=10)
 
+    textbox1 = tk.Entry(root, width=30)
+    textbox1.grid(row=1, column=1, padx=10, pady=10)
 
-root.mainloop()
+    textbox2 = tk.Entry(root, width=30)
+    textbox2.grid(row=2, column=1, padx=10, pady=10)
+    textbox2.config(show="*")
+
+    button1 = tk.Button(root, text="Access", command=access,)
+    button1.grid(row=3, column=1, padx=10, pady=10)
+
+    root.mainloop()
+
+def mainWindow():
+    root = tk.Tk()
+    root.title("MNGT System")
+    window_width = 1280
+    window_height = 720
+
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    x = (screen_width // 2) - (window_width // 2)
+    y = (screen_height // 2) - (window_height // 2)
+    root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+    label1 = tk.Label(root, text="Welcome to MNGT System\n More features coming soon...", font=("Arial", 24))
+    label1.grid(row=1, column=1, padx=10, pady=10)
+
+    root.mainloop()
